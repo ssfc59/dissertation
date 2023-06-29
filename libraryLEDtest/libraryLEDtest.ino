02000a14278ba19009a9634a8e22cc4fece9e9b3
Sophia Chong <zcrhsfc@ucl.ac.uk>
2023-06-29 16:29:55 +0100

    change in LED test

--- libraryLEDtest/libraryLEDtest.ino
+++ libraryLEDtest/libraryLEDtest.ino
@@ -1,5 +1,5 @@
 #include <FastLED.h>
-#define NUM_LEDS 48     //Number of RGB LED beads
+#define NUM_LEDS 54     //Number of RGB LED beads
 #define DATA_PIN D8    //The pin for controlling RGB LED
 #define LED_TYPE WS2812B    //RGB LED strip type
 CRGB leds[NUM_LEDS];    //Instantiate RGB LED
@@ -16,18 +16,13 @@ void loop() {
   int display = areaHumidity - libHumidity;
   //display = 16!
   Serial.println(display);
- if ((display > 10) && (display <= 16)) {
-  leds[7] = CRGB (150, 150, 150);
-  leds[8] = CRGB (150, 150, 150); //was 8&9
-  //leds[8] = CRGB(random(0,255),random(0,255),random(0,255));
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i]= CRGB (255, 255, 255);
+  }
+ 
   FastLED.show();
   Serial.println("ok");
-  }
-  else{
-     leds[8] = CRGB (0,200,0);
-       Serial.println("skill issue");
-    }
-  delay(10000000);
+  delay(1000);
 /*
    for (int i = 7; i <= 9; i++) {
     leds[i] = CRGB (100, 100, 100);
