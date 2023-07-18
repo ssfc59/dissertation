1e6e0e9c8113ede5d62d00034380b9a78bc6d40c
Sophia Chong <zcrhsfc@ucl.ac.uk>
2023-07-18 17:17:10 +0100

    did switch case for all leds

--- libraryLEDtest/libraryLEDtest.ino
+++ libraryLEDtest/libraryLEDtest.ino
@@ -6,6 +6,8 @@ CRGB leds[NUM_LEDS];    //Instantiate RGB LED
 
 const int libHumidity = 0;
 const int areaHumidity = 20;
+const int libTemp = 20;
+const int areaTemp = 20;
 
 void setup() { 
   Serial.begin(115200);
@@ -14,10 +16,111 @@ void setup() {
 
 void loop() {
   int display = libHumidity - areaHumidity;
-  
-  //display = -16!
+  int tempdisplay = libTemp - areaTemp;
+
   Serial.println(display);
 
+  if (tempdisplay <= 0) {
+    //COOLER INSIDE THE LIBRARY
+       for (int i = 92; i <= 102; i++) {
+    leds[i] = CRGB (255, 255, 255);
+    FastLED.show();
+    }
+    }
+    else if (tempdisplay > 0) {
+       for (int i = 109; i <= 122; i++) {
+    leds[i] = CRGB (255, 255, 255);
+    FastLED.show();
+    }
+      }
+switch (tempdisplay){
+    case -5:
+    case 5:
+     for (int i = 128; i <= 130; i++) {
+    leds[i] = CRGB (255, 255, 255);
+    FastLED.show();
+    }
+    break; //end case light up 5
+     
+    case -6:
+    case 6:
+     for (int i = 133; i <= 135; i++) {
+    leds[i] = CRGB (255, 255, 255);
+    FastLED.show();
+    }
+    break; //end case light up 6
+    
+   case -7:
+   case 7:
+     for (int i = 138; i <= 140; i++) {
+    leds[i] = CRGB (255, 255, 255);
+    FastLED.show();
+    }
+    break; //end case light up 7
+
+    case -8:
+    case 8:
+     for (int i = 143; i <= 145; i++) {
+    leds[i] = CRGB (255, 255, 255);
+    FastLED.show();
+    }
+    break; //end case light up 8
+
+     case -50 ... -9:
+    case 9 ... 50:
+     for (int i = 148; i <= 151; i++) {
+    leds[i] = CRGB (255, 255, 255);
+    FastLED.show();
+    }
+    break; //end case light up 8
+
+   case -4:
+   case 4:
+     for (int i = 169; i <= 170; i++) {
+    leds[i] = CRGB (255, 255, 255);
+    FastLED.show();
+    }
+    break; //end case light up 4
+
+    case -3:
+   case 3:
+     for (int i = 174; i <= 176; i++) {
+    leds[i] = CRGB (255, 255, 255);
+    FastLED.show();
+    }
+    break; //end case light up 3
+    
+   case -2:
+   case 2:
+     for (int i = 179; i <= 181; i++) {
+    leds[i] = CRGB (255, 255, 255);
+    FastLED.show();
+    }
+    break; //end case light up 2
+
+   case -1:
+   case 1:
+     for (int i = 184; i <= 186; i++) {
+    leds[i] = CRGB (255, 255, 255);
+    FastLED.show();
+    }
+    break; //end case light up 1
+
+   case 0:
+     for (int i = 189; i <= 191; i++) {
+    leds[i] = CRGB (255, 255, 255);
+    FastLED.show();
+    }
+    break; //end case light up 1
+
+   default:
+      for(int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB(100,100,0); //yellow
+      FastLED.show();
+  }
+      Serial.print("Sensor error");
+  }
+  
  if (display <= 0) {
   //LESS HUMID
    for (int i = 13; i <= 19; i++) {
@@ -37,15 +140,13 @@ void loop() {
     case -100 ... -45:
     case 45 ... -100:
      for (int i = 32; i <= 35; i++) {
-      // for (int i = 31; i <= 35; i++)
-      //80 83 is 20
     leds[i] = CRGB (255, 255, 255);
     FastLED.show();
     }
     break; //end case light up 45 
 
    case -44 ... -40:
-    case 40 ... 44:
+   case 40 ... 44:
      for (int i = 38; i <= 41; i++) {
     leds[i] = CRGB (255, 255, 255);
     FastLED.show();
@@ -54,7 +155,7 @@ void loop() {
 
     
    case -39 ... -35:
-    case 35 ... 39:
+   case 35 ... 39:
      for (int i = 42; i <= 45; i++) {
     leds[i] = CRGB (255, 255, 255);
     FastLED.show();
@@ -125,26 +226,8 @@ void loop() {
   }
       Serial.print("Sensor error");
       } //switch display
- 
-  
+
+ delay(5000);
 }
-
-// for(int i = 0; i < NUM_LEDS; i++)
-/*
-   for (int i = 7; i <= 9; i++) {
-    leds[i] = CRGB (100, 100, 100);
-    FastLED.show();
-    }
-  
-   for(int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB(255,255,255);
-      FastLED.show();
-      }
-
-    for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i]= CRGB (255, 255, 255);
-  }
- 
-  */
 
   //leds[0] = CRGB(random(0,255),random(0,255),random(0,255));    // LED shows a randomly mixed color
