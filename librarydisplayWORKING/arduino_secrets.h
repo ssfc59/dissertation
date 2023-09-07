f9e921945ca8ab6dbda84df59d74d7e57ecfe901
Sophia Chong <zcrhsfc@ucl.ac.uk>
2023-09-07 23:05:30 +0100

    Omitted values

--- librarydisplayWORKING/arduino_secrets.h
+++ librarydisplayWORKING/arduino_secrets.h
@@ -1,12 +1,12 @@
 //-------------WiFi settings-----------------------------
-const char* ssid = "UCL_IoT";
-const char* password = "pyds376Xnt";
+const char* ssid = "REPLACE-THIS-WITH-YOUR-WIFI-NAME";
+const char* password = "REPLACE-THIS-WITH-YOUR-WIFI-PASSWORD";
 
 //-------------API user settings--------------------------
-const char* clientId = "7338eda7ea694cad61cee72f9408af6f";
-const char* clientSecret = "402e2616c73769757358630e92924062";
-const char* APIurl = "https://api-metoffice.apiconnect.ibmcloud.com/v0/forecasts/point/hourly?excludeParameterMetadata=true&includeLocationName=true&latitude=51.5427233252536&longitude=0.004183041568396564";
+const char* clientId = "REPLACE-WITH-YOUR-CLIENT-ID";
+const char* clientSecret = "REPLACE-WITH-YOUR-CLIENT-SECRET";
+const char* APIurl = "REPLACE-WITH-YOUR-API-URL";
 
 //-----------Initial State user settings------------------
-const char* accesskey = "ist_ianAvJO129YcntUonQFr0RXU289WelB8";
-const char* bucketkey = "MWVEALE8NWGA";
+const char* accesskey = "REPLACE-WITH-YOUR-ACCESS-KEY";
+const char* bucketkey = "REPLACE-WITH-YOUR-BUCKET-KEY";
